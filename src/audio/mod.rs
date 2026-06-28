e505e48f5e7b260b31853c85b397ae507efd1e17
webcatzz <71499717+webcatzz@users.noreply.github.com>
2026-06-28 16:59:15 +0400

    feat!: remove `AudioSource` and `AudioPlayer`

--- src/audio/mod.rs
+++ src/audio/mod.rs
@@ -3,13 +3,9 @@
 mod audio;
 mod device;
 mod format;
-mod player;
-mod source;
 mod stream;
 
 pub use audio::*;
 pub use device::*;
 pub use format::*;
-pub use player::*;
-pub use source::*;
 pub use stream::*;
