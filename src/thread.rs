e499b24f71a2cd36411c51119d996676445169a2
webcatzz <71499717+webcatzz@users.noreply.github.com>
2026-07-02 12:59:36 +0400

    doc: define "main thread"

--- src/thread.rs
+++ src/thread.rs
@@ -3,6 +3,13 @@
 use sdl3_sys::init::SDL_IsMainThread;
 
 /// Returns `true` if called on the main thread.
+///
+/// - On Apple platforms, the main thread is the one that runs the program's
+///   entry point.
+/// - On other platforms, the main thread is the one that creates a [`Window`],
+///   which should usually be the one that runs the program's entry point.
+///
+/// [`Window`]: crate::window::Window
 pub fn is_main() -> bool {
 	unsafe { SDL_IsMainThread() }
 }
